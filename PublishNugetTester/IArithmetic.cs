bd8d634e53704c7074aa3f8185c06d2cd535067a
wrobel604 <wrobel604@gmail.com>
2025-06-20 18:32:30 +0200

    Dodaj pliki projektów.

---
+++ PublishNugetTester/IArithmetic.cs
@@ -0,0 +1,10 @@
+﻿namespace PublishNugetTester
+{
+    public interface IArithmetic<T>
+    {
+        T Addition(T left, T right);
+        T Subtraction(T left, T right);
+        T Multiplication(T left, T right);
+        T Division(T left, T right);
+    }
+}
